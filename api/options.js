bd7ab1062188cf2b743f84cf6e53a0993bf4250d
Andrew Leonard <leonardishere@gmail.com>
2022-03-16 13:55:07 -0700

    switch options function to node

---
+++ api/options.js
@@ -0,0 +1,15 @@
+// Define headers
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT,DELETE"
+}
+
+// Lambda handler
+exports.handler = async function(event) {
+  return {
+      statusCode: 200,
+      headers: headers
+    }
+  }
+}
