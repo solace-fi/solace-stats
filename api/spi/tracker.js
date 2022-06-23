a886e7a01dd5be79b5ef9bd24ad84727b07a389a
Andrew Leonard <leonardishere@gmail.com>
2022-06-23 16:13:35 -0700

    update to SPI v3

--- api/spi/tracker.js
+++ api/spi/tracker.js
@@ -2,20 +2,22 @@
 
 const { getProvider, getMulticallProvider, s3GetObjectPromise, s3PutObjectPromise, snsPublishError, withBackoffRetries, formatTimestamp, fetchBlock } = require("./../utils/utils")
 
-const { track_ethereum_v1 } = require("./ethereum_v1")
-const { track_polygon_v2 } = require("./polygon_v2")
-const { track_fantom_v2 } = require("./fantom_v2")
+const { track_ethereum_v3 } = require("./ethereum_v3")
+const { track_aurora_v3 } = require("./aurora_v3")
+const { track_polygon_v3 } = require("./polygon_v3")
+const { track_fantom_v3 } = require("./fantom_v3")
 
 async function track_policies() {
   return new Promise(async (resolve) => {
     console.log('start tracking all policies')
-    let [ethereum_v1, polygon_v2, fantom_v2] = await Promise.all([
-      track_ethereum_v1(),
-      track_polygon_v2(),
-      track_fantom_v2(),
+    let [ethereum_v3, aurora_v3, polygon_v3, fantom_v3] = await Promise.all([
+      track_ethereum_v3(),
+      track_aurora_v3(),
+      track_polygon_v3(),
+      track_fantom_v3(),
     ])
     console.log('done tracking all policies')
-    let res = {ethereum_v1, polygon_v2, fantom_v2}
+    let res = {ethereum_v3, aurora_v3, polygon_v3, fantom_v3}
     resolve(res)
   })
 }
